271ed086ff2f13b25ebe64e88177970be3de6e0d
rocca76 <aubin.eric@gmail.om>
2012-06-26 22:40:41 -0400

    Initial commit

--- NetduinoBridgeWPF/Netduino.Communication/Properties/AssemblyInfo.cs
+++ NetduinoBridgeWPF/Netduino.Communication/Properties/AssemblyInfo.cs
@@ -5,14 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NetduinoApplication1")]
+[assembly: AssemblyTitle("Netduino.Communications")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NetduinoApplication1")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyProduct("Netduino.Communications")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a9c7ff9f-463e-48ff-9a73-6d457a62af39")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -21,5 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NetduinoBridgeWPF/NetduinoBridgeWPF/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows;
+using Netduino.Communication;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Threading;
+using System.Timers;
+
+namespace NetduinoBridgeWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static Action<System.Action> executor = action => action();
+        private CommunicateWithNetduino _ethernetCommunication = CommunicateWithNetduino.GetInstance();
+        private static System.Timers.Timer aTimer;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Subscribing to events
+            _ethernetCommunication.EventHandlerMessageReceived += new EventHandler<MessageEventArgs>(OnMessageReceived);
+            _ethernetCommunication.EventHandlerStatusUpdate += new EventHandler<MessageEventArgs>(OnStatusUpdate);
+            _ethernetCommunication.StartListening(null);
+
+            // Connect must be called after the blocking accept
+            aTimer = new System.Timers.Timer(500);
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.AutoReset = false;
+            aTimer.Enabled = true;
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            SendTimeToNetdino();
+        }
+
+        private void OnMessageReceived(object sender, MessageEventArgs e)
+        {
+            Debug.Print(e.Message);
+        }
+
+        private void OnStatusUpdate(object sender, MessageEventArgs e)
+        {
+            Debug.Print(e.Message);
+        }
+
+        private void SendTimeToNetdino()
+        {
+            string presentTime = string.Format("TIME {0} {1} {2} {3} {4} {5} {6}",
+                                                DateTime.Now.Year,
+                                                DateTime.Now.Month,
+                                                DateTime.Now.Day,
+                                                DateTime.Now.Hour,
+                                                DateTime.Now.Minute,
+                                                DateTime.Now.Second,
+                                                DateTime.Now.Millisecond);
+
+            _ethernetCommunication.SendMessage(presentTime);
+        }
+
+        private void SendExitToNetdino()
+        {
+            _ethernetCommunication.SendMessage("EXIT");
+        }
+    }
+}
